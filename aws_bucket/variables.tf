9b2a5010b284ecb8d30389114ec2b5d0c8e07d43
Hector Huertas <hectorhuertas@gmail.com>
2021-09-14 13:07:23 +0200

    Add bucket versioning and expiration

--- aws_bucket/variables.tf
+++ aws_bucket/variables.tf
@@ -8,6 +8,36 @@ variable "public" {
   default     = false
 }
 
+variable "object_expiration" {
+  description = "Enable object deletion after a defined amount of days"
+  type        = bool
+  default     = false
+}
+
+variable "object_expiration_days" {
+  description = "Amount of days before expiration"
+  type        = number
+  default     = 30
+}
+
+variable "versioning" {
+  description = "Enable versioning for bucket objects"
+  type        = bool
+  default     = false
+}
+
+variable "version_expiration" {
+  description = "Enable version deletion after a defined amount of days"
+  type        = bool
+  default     = false
+}
+
+variable "version_expiration_days" {
+  description = "Amount of days before expiration of past versions"
+  type        = number
+  default     = 30
+}
+
 locals {
   # bucket name that complies with UW's permission boundaries
   prefixed_name = "uw-${local.caller_account_alias}-${local.caller_team}-${var.name}"
