c47ae22a0998cc6f259ab982f003ef9825670663
Hector Huertas <hectorhuertas@gmail.com>
2022-03-21 10:16:14 +0100

    Remove experimental cluster knowledge
    Instead of accepting a bool to detect an exp cluster, take a complete
    auth policy as input. This way the module is not aware of exp clusters
    and it's more flexible, since it allows customized auth for roles.

--- aws_bucket_access/variables.tf
+++ aws_bucket_access/variables.tf
@@ -2,19 +2,19 @@ variable "bucket_id" {
   description = "Id of the bucket to be accessed"
 
   validation {
-    condition = substr(var.bucket_id, 0, 3) == "uw-"
+    condition     = substr(var.bucket_id, 0, 3) == "uw-"
     error_message = "The bucket_id must follow the format 'uw-<environment>-<team>-*'."
   }
 }
 
 variable "access_method" {
   type        = string
-  description = "Type of bucket access. Can be 'vault_role' or 'iam_user'."
-  default     = "vault_role"
+  description = "Type of bucket access. Can be 'iam_role' or 'iam_user'."
+  default     = "iam_role"
 
   validation {
-    condition     = contains(["vault_role", "iam_user"], var.access_method)
-    error_message = "The access_type must be one of: vault_role, iam_user."
+    condition     = contains(["iam_role", "iam_user"], var.access_method)
+    error_message = "The access_type must be one of: iam_role, iam_user."
   }
 }
 
@@ -24,14 +24,14 @@ variable "write_access" {
   default     = false
 }
 
-variable "exp_cluster" {
-  description = "Is the bucket to be accessed from an experimental cluster?"
-  type        = bool
-  default     = false
+variable "iam_role_auth_policy" {
+  description = "Auth policy for the iam_role. If left blank, only vault would be able to assume the role"
+  type        = string
+  default     = ""
 }
 
 locals {
   bucket_name_without_uw       = trimprefix(var.bucket_id, "uw-")
-  bucket_name_without_env     = trimprefix(local.bucket_name_without_uw, local.caller_account_alias)
+  bucket_name_without_env      = trimprefix(local.bucket_name_without_uw, local.caller_account_alias)
   bucket_name_without_prefixes = trimprefix(local.bucket_name_without_env, "-${local.caller_team}-")
 }
