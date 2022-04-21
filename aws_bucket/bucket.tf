eccd5422b2029d6f1cc049a8b1875687089da233
Hector Huertas <hectorhuertas@gmail.com>
2022-04-21 13:14:49 +0200

    Update aws bucket to aws provider v4

--- aws_bucket/bucket.tf
+++ aws_bucket/bucket.tf
@@ -1,55 +1,16 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = local.prefixed_name
-  acl    = var.public ? "public-read" : "private"
-
-  # Ensure that bucket is encrypted at rest
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  # Expiration of bucket objects
-  dynamic "lifecycle_rule" {
-    for_each = var.object_expiration ? [1] : []
-    content {
-      id      = "object-expiration"
-      enabled = var.object_expiration
-
-      expiration {
-        days = var.object_expiration_days
-      }
-    }
-  }
-
-  # Enable versioning
-  dynamic "versioning" {
-    for_each = var.versioning ? [1] : []
-    content {
-      enabled = var.versioning
-    }
-  }
-
-  # Expiration of old versions of the objects
-  dynamic "lifecycle_rule" {
-    for_each = var.version_expiration ? [1] : []
-    content {
-      id      = "version-expiration"
-      enabled = var.version_expiration
-
-      noncurrent_version_expiration {
-        days = var.version_expiration_days
-      }
-    }
-  }
 
   # tags help with cost allocation
   tags = {
     Name  = local.prefixed_name
     owner = local.caller_team
   }
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = var.public ? "public-read" : "private"
 }
 
 # If bucket is private, block public access to comply with security audits
@@ -59,6 +20,55 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
 
   block_public_acls   = true
   block_public_policy = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  count  = var.versioning ? 1 : 0
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
+  count  = var.version_expiration || var.object_expiration ? 1 : 0
+  bucket = aws_s3_bucket.bucket.id
+
+  # Expiration of bucket objects
+  dynamic "rule" {
+    for_each = var.object_expiration ? [1] : []
+    content {
+      id     = "object-expiration"
+      status = "Enabled"
+
+      expiration {
+        days = var.object_expiration_days
+      }
+    }
+  }
+
+  # Expiration of old versions of the objects
+  dynamic "rule" {
+    for_each = var.version_expiration ? [1] : []
+    content {
+      id     = "version-expiration"
+      status = "Enabled"
+
+      noncurrent_version_expiration {
+        noncurrent_days = var.version_expiration_days
+      }
+    }
+  }
 }
 
 output "bucket" {
