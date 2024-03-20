d9b1c4e018b7992aba16dbe3457047b0f1c9beb7
marcinginszt <marcinginszt@gmail.com>
2024-03-20 14:09:21 +0100

    additional permissions for bucket access role

--- aws_bucket_access/policies.tf
+++ aws_bucket_access/policies.tf
@@ -2,7 +2,11 @@ data "aws_iam_policy_document" "ro" {
   statement {
     actions = [
       "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListBucketVersions",
       "s3:GetObject",
+      "s3:ListMultipartUploadParts",
     ]
     resources = [
       "arn:aws:s3:::${var.bucket_id}",
@@ -15,9 +19,14 @@ data "aws_iam_policy_document" "rw" {
   statement {
     actions = [
       "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListBucketVersions",
       "s3:GetObject",
       "s3:PutObject",
       "s3:DeleteObject",
+      "s3:AbortMultipartUpload",
+      "s3:ListMultipartUploadParts",
     ]
     resources = [
       "arn:aws:s3:::${var.bucket_id}",
