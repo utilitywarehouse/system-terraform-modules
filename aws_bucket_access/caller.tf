c4902e71379de8ebdfa17c6f1e77be6ac72441f4
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2025-07-01 17:24:27 +0300

    Added exp account id

--- aws_bucket_access/caller.tf
+++ aws_bucket_access/caller.tf
@@ -4,6 +4,7 @@ locals {
   uw_account_aliases_map = {
     950135041896 = "dev"
     703452047160 = "prod"
+    229806150680 = "explore"
   }
   caller_account_id    = data.aws_caller_identity.current.account_id
   caller_account_alias = local.uw_account_aliases_map[local.caller_account_id]
