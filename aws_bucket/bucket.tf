9af21c21aded6cb7cc234b5fa986f0bbc051af71
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2025-08-11 14:36:22 +0300

    commented the encryption part

--- aws_bucket/bucket.tf
+++ aws_bucket/bucket.tf
@@ -39,15 +39,15 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.bucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+# resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+#   bucket = aws_s3_bucket.bucket.bucket
+#
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
 resource "aws_s3_bucket_versioning" "versioning" {
   count  = var.versioning ? 1 : 0
